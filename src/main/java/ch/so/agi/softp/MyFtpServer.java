a99f9226b0ae126132c192a37d948e799c685738
Stefan Ziegler <edi.gonzales@gmail.com>
2020-11-01 11:20:20 +0100

    wip

--- src/main/java/ch/so/agi/softp/MyFtpServer.java
+++ src/main/java/ch/so/agi/softp/MyFtpServer.java
@@ -1,12 +1,19 @@
 package ch.so.agi.softp;
 
+import java.net.URI;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.VFS;
 import org.apache.ftpserver.ConnectionConfigFactory;
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.FileSystemFactory;
+import org.apache.ftpserver.ftplet.FileSystemView;
 import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.User;
 import org.apache.ftpserver.ftplet.UserManager;
 import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
@@ -14,6 +21,8 @@ import org.apache.ftpserver.usermanager.impl.BaseUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.vfsutils.ftpserver.filesystem.VfsAuthenticator;
+import org.vfsutils.ftpserver.filesystem.VfsFileSystemFactory;
 
 @Component
 public class MyFtpServer {
@@ -32,7 +41,7 @@ public class MyFtpServer {
         
         BaseUser user = new BaseUser();
         user.setName("anonymous");
-        user.setHomeDirectory("/tmp");
+//        user.setHomeDirectory("/tmp");
 
         try {
             userManager.save(user);
@@ -41,7 +50,14 @@ public class MyFtpServer {
             return;
         }
         
-        
+        try {
+            for (String schema: VFS.getManager().getSchemes()) {
+                log.info(schema);
+            }
+        } catch (FileSystemException e2) {
+            e2.printStackTrace();
+        }
+
         
         
         ListenerFactory listenerFactory = new ListenerFactory();
@@ -56,15 +72,42 @@ public class MyFtpServer {
         factory.setConnectionConfig(connectionConfigFactory.createConnectionConfig());
         factory.setUserManager(userManager);
         factory.addListener("default", listenerFactory.createListener());
-//        factory.setFileSystem(fileSystem);
-         
-        FtpServer server = factory.createServer();
+        
+        // http://vfs-utils.sourceforge.net/ftpserver/apidocs/index.html
+        VfsFileSystemFactory vfsFileSystemFactory = new VfsFileSystemFactory();
+        BaseUser vfsUser = new BaseUser();
+        vfsUser.setName("vaso");
+        vfsUser.setPassword("vaso123");
+        
+        VfsAuthenticator vfsAuthentificator = new VfsAuthenticator();
+        vfsAuthentificator.setVfsRoot("ftp://vaso:vaso123@ftp.infogrips.ch");
+        vfsAuthentificator.setVfsType("virtual");
         try {
-            server.start();
-        } catch (FtpException e) {
-            log.error("failed to start ftp server:", e);
-            return;
+            vfsAuthentificator.authenticate("vaso", "vaso123", "/");
+        } catch (FileSystemException e) {
+            e.printStackTrace();
         }
+        vfsFileSystemFactory.setAuthenticator(vfsAuthentificator);
+        
+//        vfsUser.setHomeDirectory("ftp://anonymous:foo.ch@ftp.swfwmd.state.fl.us");
+//        try {
+//            vfsFileSystemFactory.createFileSystemView(vfsUser);
+//        } catch (FtpException e) {
+//            log.error("could not create vfs file system", e);
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        factory.setFileSystem(vfsFileSystemFactory);
+//         
+//        FtpServer server = factory.createServer();
+//        
+//        try {
+//            server.start();
+//        } catch (FtpException e) {
+//            log.error("failed to start ftp server:", e);
+//            return;
+//        }
 
 
     }
